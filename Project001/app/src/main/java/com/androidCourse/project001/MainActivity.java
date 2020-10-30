be5f456973250599606d5d037d721ce3ee6d2a6b
Charles Vaghetti <63205923+vagh3tt1@users.noreply.github.com>
2020-10-30 14:30:15 -0300

    Revert "Revert "SumTest""
    This reverts commit d8cf444e7d802bedf3bb27a1858b47ef7891c370.

--- Project001/app/src/main/java/com/androidCourse/project001/MainActivity.java
+++ Project001/app/src/main/java/com/androidCourse/project001/MainActivity.java
@@ -1,8 +1,12 @@
 package com.androidCourse.project001;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void alterarTexto (View view){
+
+        TextView nomeQualquer = findViewById(R.id.alteracaoTexto);
+        nomeQualquer.setText("Texto Modificado");
+
     }
 }
